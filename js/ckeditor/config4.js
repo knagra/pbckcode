73efbbad01c7541d0ffdaf5c2b828aed247ae216
Pierre Baron <prbaron22@gmail.com>
2013-04-15 22:58:52 +0200

    improve documentation

--- js/ckeditor/config4.js
+++ js/ckeditor/config4.js
@@ -1,11 +1,11 @@
 CKEDITOR.editorConfig = function( config ) {
 	config.toolbarGroups = [
 		{ name: 'document', groups: [ 'mode'] }, // shows the source button
-		{ name: 'pbckcode' },                    // shows the pbckcode button
+		{ name: 'pbckcode' }                     // shows the pbckcode button
 	];
 	config.extraPlugins = 'pbckcode,onchange';
 	config.pbckcode = {
-		theme : "monokai",
-		highlighter : "PRETTIFY"
+		modes :  [ ["Java" , "java"], ["Markdown" , "markdown"] ],
+		highlighter : "HIGHLIGHT"
 	};
 };

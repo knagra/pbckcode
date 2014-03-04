34dabd3da6a74d1460d7888d0aa5a1ca817a80d3
Pierre Baron <prbaron22@gmail.com>
2014-03-04 19:45:27 +0100

    maj des exemples

--- js/examples.js
+++ js/examples.js
@@ -1,71 +1,69 @@
-jQuery(function($){
-    prettyPrint();
+prettyPrint();
 
-    // DEFAULT CONFIGURATION
-    CKEDITOR.replace('editor1', {
-        customConfig: 'config1.js'
-    });
+// DEFAULT CONFIGURATION
+CKEDITOR.replace('editor1', {
+    customConfig: 'config1.js'
+});
 
-    // PRETTIFY
-    CKEDITOR.replace('editor2', {
-        customConfig: 'config2.js'
-    });
+// PRETTIFY
+CKEDITOR.replace('editor2', {
+    customConfig: 'config2.js'
+});
 
-    // PRISM
-    CKEDITOR.replace('editor3', {
-        customConfig: 'config3.js'
-    });
+// PRISM
+CKEDITOR.replace('editor3', {
+    customConfig: 'config3.js'
+});
 
-    // HIGHLIGHT
-    CKEDITOR.replace('editor4', {
-        customConfig: 'config4.js'
-    });
+// HIGHLIGHT
+CKEDITOR.replace('editor4', {
+    customConfig: 'config4.js'
+});
 
-    // SYNTAX_HIGHLIGHTER
-    CKEDITOR.replace('editor5', {
-        customConfig: 'config5.js'
-    });
+// SYNTAX_HIGHLIGHTER
+CKEDITOR.replace('editor5', {
+    customConfig: 'config5.js'
+});
 
 
-    /***************************************/
-    // this part is for demo purpose only
-    /***************************************/
+/***************************************/
+// this part is for demo purpose only
+/***************************************/
 
-    // DEFAULT CONFIGURATION
-    CKEDITOR.instances.editor1.on('change', function(e) {
-        // append the result into the div
-        $('#output1').html(e.editor.getData());
-    });
+// DEFAULT CONFIGURATION
+CKEDITOR.instances.editor1.on('change', function(e) {
+    // append the result into the div
+    document.getElementById('output1').innerHTML = e.editor.getData();
+});
 
-    // PRETTIFY
-    CKEDITOR.instances.editor2.on('change', function(e) {
-        // append the result into the div
-        $('#output2').html(e.editor.getData());
+// PRETTIFY
+CKEDITOR.instances.editor2.on('change', function(e) {
+    // append the result into the div
+    document.getElementById('output2').innerHTML = e.editor.getData();
 
-        prettyPrint(null, document.getElementById("output2"));
-    });
+    prettyPrint(null, document.getElementById('output2'));
+});
 
-    // PRISM
-    CKEDITOR.instances.editor3.on('change', function(e) {
-        // append the result into the div
-        $('#output3').html(e.editor.getData());
+// PRISM
+CKEDITOR.instances.editor3.on('change', function(e) {
+    // append the result into the div
+    document.getElementById('output3').innerHTML = e.editor.getData();
 
-        Prism.highlightAll();
-    });
+    Prism.highlightAll();
+});
 
-    // HIGHLIGHT
-    CKEDITOR.instances.editor4.on('change', function(e) {
-        // append the result into the div
-        $('#output4').html(e.editor.getData());
+// HIGHLIGHT
+CKEDITOR.instances.editor4.on('change', function(e) {
+    // append the result into the div
+    document.getElementById('output4').innerHTML = e.editor.getData();
 
-        $('#output4').find('code').each(function(i, e) {hljs.highlightBlock(e); });
-    });
+    hljs.highlightBlock(document.getElementById('output4').children[0].children[0]);
+});
 
-    // SYNTAX_HIGHLIGHTER
-    CKEDITOR.instances.editor5.on('change', function(e) {
-        // append the result into the div
-        $('#output5').html(e.editor.getData());
+// SYNTAX_HIGHLIGHTER
+CKEDITOR.instances.editor5.on('change', function(e) {
+    // append the result into the div
+    document.getElementById('output5').innerHTML = e.editor.getData();
 
-        SyntaxHighlighter.highlight();
-    });
+    SyntaxHighlighter.highlight();
 });
